6d768ec2f8898901b1f5064f0eec8f8f814aca35
Ed Beaty <ebeaty@cisco.com>
2025-05-12 01:02:36 -0700

    .

--- topo/node/cisco/cisco.go
+++ topo/node/cisco/cisco.go
@@ -671,6 +671,9 @@ func processConfig(cfg string) string {
 }
 
 func (n *Node) ConfigPush(ctx context.Context, r io.Reader) error {
+	if n.Proto.Model == ModelXRD {
+		return status.Errorf(codes.Unimplemented, "config push is not implemented for cisco xrd node")
+	}
 
 	log.Infof("%s - pushing config", n.Name())
 
